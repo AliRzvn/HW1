d6420c1656800372aae78e18327612df540b674e
AliRzvn <81423543+AliRzvn@users.noreply.github.com>
2021-03-28 14:22:05 -0700

    Add files via upload

---
+++ relu.py
@@ -0,0 +1,28 @@
+import numpy as np
+from module import Module
+
+
+class ReLU(Module):
+    def __init__(self, name):
+        super(ReLU, self).__init__(name)
+
+    def forward(self, x, **kwargs):
+        """
+        x: input array.
+        out: output of ReLU function for input x.
+        **Save whatever you need for backward pass in self.cache.
+        """
+        out = None
+        # todo: implement the forward propagation for ReLU module
+   
+        return out
+
+    def backward(self, dout):
+        """
+        dout: gradients of Loss w.r.t. this layer's output.
+        dx: gradients of Loss w.r.t. this layer's input.
+        """
+        dx = None
+        # todo: implement the backward propagation for ReLU module.
+  
+        return dx
